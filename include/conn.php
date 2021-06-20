18f20304b86e9218cbd9c8ad38214781cbc264a4
hoang minh thanh <thanhhmbhaf190135@fpt.edu.vn>
2021-06-20 17:17:33 +0700

    edit conect

--- include/conn.php
+++ include/conn.php
@@ -1,13 +1,13 @@
 
 	<?php
 	$server = "52.3.220.13";
-	$user = "thanh123";
-	$pass = "thanh123";
-	$database = "website";
+	$user = "thanh090901";
+	$pass = "thanh090901";
+	$database = "computing";
 	$connect = mysqli_connect($server, $user, $pass, $database); 
-	if (!$connect) {
-		die("Connect Failed:".mysqli_connect_error());
-		# code...
-	}
+	 //if (!$connect) {
+	  // die("Connect Failed:".mysqli_connect_error());
+	 	# code...
+	// }
 	mysqli_set_charset($connect, 'UTF8');
 ?>
