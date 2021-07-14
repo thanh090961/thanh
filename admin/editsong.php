75a76b648d28a307999353330f1f3d4b7ed33085
hoang minh thanh <thanhhmbhaf190135@fpt.edu.vn>
2021-07-14 09:06:09 +0700

    edit text

--- admin/editsong.php
+++ admin/editsong.php
@@ -12,7 +12,7 @@
 <!-- menu -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-	  <a class="navbar-brand" href="#">Navbar</a>
+	  <a class="navbar-brand" href="#">ATN</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 	?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container">
-	  <a class="navbar-brand" href="#"></a>
+	  <a class="navbar-brand" href="#">ATN</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
